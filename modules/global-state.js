c5aa16447ab55408810b1c28255bc26b4db0f47f
Gabriel Urbaniak <gurbaniak@internships.antmicro.com>
2023-09-19 14:13:01 +0200

    Add a global state class used to track acquired sections

---
+++ modules/global-state.js
@@ -0,0 +1,19 @@
+export class State {
+    static #instance;
+    
+    static instance() {
+        if (State.#instance == null) {
+            State.#instance = new State();
+        }
+        return State.#instance;
+    }
+    static reset() {
+        State.#instance = new State();
+    }
+
+    sections = []
+
+    constructor() {
+        sections = [];
+    }
+}
