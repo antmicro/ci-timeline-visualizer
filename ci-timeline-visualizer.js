85a29b037cfc7928e6e7c9f6c98ea3a1a11b2a46
Gabriel Urbaniak <gurbaniak@internships.antmicro.com>
2023-10-18 15:42:49 +0200

    [#50427] Make Orchestrator responsible for executing the visualizer

--- ci-timeline-visualizer.js
+++ ci-timeline-visualizer.js
@@ -1,22 +1,7 @@
-import { Loader } from "./modules/Loader.js";
-import { TimeManager } from "./modules/TimeManager.js";
+import { Orchestrator } from "./modules/Orchestrator.js";
 
 (async function () {
-  let loader = new Loader();
+  let orchestrator = new Orchestrator();
 
-  let fetcher;
-  let parser;
-  let sectionManager;
-  let gui;
-
-  [fetcher, parser, sectionManager, gui] = await loader.load();
-  await TimeManager.setup(sectionManager);
-
-  fetcher.setListener((data) => {parser.parse(data)});
-  parser.listener = (data) => {sectionManager.addSectionsFromTags(data)};
-  sectionManager.listeners.push((data) => {gui.update(data)});
-
-  fetcher.startPolling();
-  sectionManager.createPendingSection();
-  gui.startPolling();
+  await orchestrator.start();
 }());
