538fd226c41aa1ddc821939e559e756a94ece969
Gabriel Urbaniak <gurbaniak@internships.antmicro.com>
2023-12-05 11:38:37 +0100

    [#50427] Extract getting runner time offset in TimeManager

--- modules/TimeManager.js
+++ modules/TimeManager.js
@@ -19,21 +19,8 @@ export class TimeManager {
     }
 
     static runnerEpoch() {
-        if(TimeManager.runnerTimeOffset == null) {
- 
-            if(TimeManager.firstSection == null) {
-                console.log("[ci-timeline-visualizer] You want to get an epoch while there is no 'prepare_executor' section while in 'distrustRunnerTime' mode. \n" +
-                "Switching back to local epoch (setting 'distrustRunnerTime' to false)");
-                ConfigManager.config["distrustRunnerTime"] = false;
-                return TimeManager.localEpoch();
-            }
-
-            TimeManager.runnerTimeOffset = 
-                TimeManager.firstSection.startTimestamp -
-                Math.floor(new Date(TimeManager.jobData["started_at"]).getTime() / 1000);
-            console.log("[ci-timeline-visualizer] Runner time offset: " + TimeManager.runnerTimeOffset);
-        }
-        return TimeManager.localEpoch() + TimeManager.runnerTimeOffset;
+        let timeOffset = TimeManager.getTimeOffset();
+        return TimeManager.localEpoch() + timeOffset;
     }
 
     static async setup(sectionManager) {
@@ -47,6 +34,27 @@ export class TimeManager {
         TimeManager.jobData = await new Promise(resolve => {
             api.getJobData((response) => resolve(response));
         });
+    }
+
+    static getTimeOffset() {
+        if(!ConfigManager.config["distrustRunnerTime"])
+            return 0;
+
+        if(TimeManager.runnerTimeOffset == null) {
+            if(TimeManager.firstSection == null) {
+                console.log("[ci-timeline-visualizer] You want to get a runner time offset while there is no 'prepare_executor' section while in 'distrustRunnerTime' mode. \n" +
+                "Switching back to local epoch (setting 'distrustRunnerTime' to false)");
+                ConfigManager.config["distrustRunnerTime"] = false;
+                return 0;
+            }
+
+            TimeManager.runnerTimeOffset = 
+                TimeManager.firstSection.startTimestamp -
+                Math.floor(new Date(TimeManager.jobData["started_at"]).getTime() / 1000);
+            console.log("[ci-timeline-visualizer] Runner time offset: " + TimeManager.runnerTimeOffset);
+        }
+
+        return TimeManager.runnerTimeOffset;
     }
 
     static firstSectionCheck(sections) {
